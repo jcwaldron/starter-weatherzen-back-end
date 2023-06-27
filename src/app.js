686ba9da2690b4b3a2bffb4771382c930fb6651f
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2023-06-27 12:27:06 -0500

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const observationsRouter = require('./observations/observations.router')
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-
+app.use("/observations", observationsRouter)
 app.use(notFound);
 app.use(errorHandler);
 
